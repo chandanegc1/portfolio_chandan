03dee01dc3c0d44c3f15a3e17277df280324e0e6
CHANDAN KANAUJIA <113818031+chandanegc1@users.noreply.github.com>
2024-02-23 10:49:33 +0530

    complete

---
+++ src/Components/utils/data.js
@@ -0,0 +1,80 @@
+export const projectData = [
+  {
+    id: 1,
+    title: "Clothes Shop",
+    techStack:
+      "React.js,MongoDB, Express.js, Node.js,SASS, Axios & React-Hot-Toast",
+    imgSrc:
+      "https://github.com/abhinav2-3/Assets/blob/master/project/amazon-clone.jpeg?raw=true",
+    hrefURL: "https://shoppingwithjoy.netlify.app/",
+    ytLink: "",
+  },
+  {
+    id: 2,
+    title: "Foot Hut",
+    techStack: "React.js,MongoDB, Express.js, Node.js,SASS, Axios",
+    imgSrc:
+      "https://raw.githubusercontent.com/abhinav2-3/Assets/master/project/zomato-clone.webp",
+    hrefURL: "https://github.com/chandanegc1/Food_Delivery_App",
+    ytLink: "",
+  },
+  {
+    id: 3,
+    title: "Job Tracker App",
+    techStack: "React.js, CSS, Chart.js, Axios & HTML5",
+    imgSrc:
+      "https://static.vecteezy.com/system/resources/previews/003/070/316/large_2x/job-search-neon-signboard-on-brick-wall-free-vector.jpg",
+    hrefURL: "/",
+    ytLink: "",
+  },
+  {
+    id: 4,
+    title: "Real Time Chat App",
+    techStack: "React.js, SASS, Framer-Motion & HTML5",
+    imgSrc:"https://github.com/chandanegc1/assets/blob/main/chatapp.jpg?raw=true",
+    hrefURL: "",
+    ytLink: "", 
+  },
+];
+
+export const miniProjectsData = [
+  {
+    id: 1,
+    title: "E-Commerce",
+    techStack: "React.js,MongoDB, Express.js, Node.js,SASS, & Axios",
+    imgSrc:
+      "https://images.sftcdn.net/images/t_app-cover-l,f_auto/p/3353deec-9b52-11e6-b34b-00163ec9f5fa/3269793528/flipkart-online-shopping-app-india-screenshot.jpg",
+    hrefURL: "https://github.com/chandanegc1/Shopping_app_modern2024",
+    ytLink: "",
+  },
+  {
+    id: 2,
+    title: "Social Media Clone",
+    techStack: "React.js, HTML5 & CSS",
+    imgSrc:
+      "https://cdn.dribbble.com/users/16624/screenshots/5401619/media/ced804ca8aaa4b109dbda99a71888d4e.png?resize=1000x750&vertical=center",
+    hrefURL: "",
+    ytLink: "",
+  },
+  {
+    id: 3,
+    title: "Quize App",
+    techStack: "HTML5, CSS & Javascript",
+    imgSrc:
+      "https://cdn.dribbble.com/users/321379/screenshots/20005134/media/6aae256b393f823f80da4f2f1816dadf.png?resize=1000x750&vertical=center",
+    hrefURL: "",
+    ytLink: "",
+  },
+  {
+    id: 4,
+    title: "Form Validation",
+    techStack: "HTML5, CSS, Javascript & API",
+    imgSrc:
+      "https://cdn.dribbble.com/userupload/4267014/file/original-177be346f4279d9431626d52c2bdbb70.png?resize=400x300&vertical=center",
+    hrefURL: "",
+    ytLink: "",
+  },
+];
+
+export const resumeURL =
+  "https://drive.google.com/file/d/1udePiK4QDY03vvjWqi2LA24omEHLWLCb/view?usp=drive_link";
