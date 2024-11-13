79c8e586dde177ba54a6b77c4c7b0c6fbc0f819d
chandan <chandanegc@gmail.com>
2024-11-13 09:22:28 +0530

    experience feature almost completed

--- src/Components/Card/ExperienceCart.jsx
+++ src/Components/Card/ExperienceCart.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { PiMonitorPlayFill } from "react-icons/pi";
 import { FaExternalLinkAlt } from "react-icons/fa";
 
 const ExperienceCart = ({
@@ -10,10 +9,10 @@ const ExperienceCart = ({
   companyWebLink,
   logo,
   companyName,
-  key,
   start,
   end,
   total,
+  certificate,
 }) => {
   return (
     <div className="flex flex-col h-auto text-l_textColor dark:text-d_textColor overflow-hidden gap-2 overflow-ellipsis">
@@ -27,11 +26,25 @@ const ExperienceCart = ({
       <div className="flex flex-col px-4 py-2 h-auto">
         <figcaption className="font-bold text-xl">{companyName}</figcaption>
         {/* <p className="text-sm">{id}</p> */}
-        <p className="text-sm">{technologies}</p>
-        <p className="text-sm">{start}</p>
-        <p className="text-sm">{end}</p>
-        <p className="text-sm">{total}</p>
-        <p className="text-sm">{description}</p>
+        <section className="flex">
+          <p className="text-sm">
+            <strong>Start: </strong>
+            <i>{start}</i>{" "}
+          </p>
+          <p className="text-sm">
+            <strong>&nbsp; End: </strong>
+            <i>{end}</i>
+          </p>
+        </section>
+        <p className="text-sm">
+          <strong>Duration: </strong>
+          {total}
+        </p>
+        {technologies&&<p className="text-sm">
+          <strong>Skills: </strong>
+          {technologies}
+        </p>}
+        <p className="text-sm">"{description}"</p>
         <div className="  justify-between place-items-center h-20 md:lg:flex-row md:justify-evenly link">
           <a
             href={companyWebLink || "/"}
@@ -39,8 +52,28 @@ const ExperienceCart = ({
             className="flex place-items-center text-sm bg-l_primary dark:bg-d_primary py-1 px-4 rounded-2xl border duration-300 transition-all hover:text-red-500 box "
             rel="noreferrer"
           >
-            Link <FaExternalLinkAlt size={15} className="ml-2" />
+            Company <FaExternalLinkAlt size={15} className="ml-2" />
           </a>
+          {certificate && (
+            <a
+              href={certificate || "/"}
+              target="_blank"
+              className="flex place-items-center text-sm bg-l_primary dark:bg-d_primary py-1 px-4 rounded-2xl border duration-300 transition-all hover:text-red-500 box "
+              rel="noreferrer"
+            >
+              Certificate <FaExternalLinkAlt size={15} className="ml-2" />
+            </a>
+          )}
+          {Projects?.map((item,index) => (
+            <a key={index}
+              href={item.link || "/"}
+              target="_blank"
+              className="flex place-items-center text-sm bg-l_primary dark:bg-d_primary py-1 px-4 rounded-2xl border duration-300 transition-all hover:text-red-500 box "
+              rel="noreferrer"
+            >
+              Project:{+index+1} <FaExternalLinkAlt size={15} className="ml-2" />
+            </a>
+          ))}
         </div>
       </div>
     </div>
