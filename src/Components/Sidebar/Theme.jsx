aa740dc4a4180323f794ec298e21ea3b615ec378
chandan <chandanegc@gmail.com>
2025-03-11 18:57:58 +0530

    fix some issue

--- src/Components/Sidebar/Theme.jsx
+++ src/Components/Sidebar/Theme.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { FiMoon, FiSun } from "react-icons/fi";
 
 const Theme = () => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
 
   useEffect(() => {
     if (theme === "dark") document.documentElement.classList.add("dark");
